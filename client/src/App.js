9bdbaa23852356e6acc4a45c5ba629043e48c555
dancorrea86 <danielmacielcorrea@gmail.com>
2019-10-13 22:51:22 -0300

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 
-console.log('ff')
+import HeaderBlog from './components/HeaderBlog'
 
 class App extends Component {
   constructor(props) {
@@ -24,20 +24,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HeaderBlog />
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
     }
---
+++ client/src/components/HeaderBlog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {HeaderContainer} from 'carbon-components-react'
+
+const HeaderBlog = props => {
+  return (
+  <HeaderContainer
+    isSideNavExpanded={false}
+   render={function noRefCheck(){}}
+  />
+  )
+}
+
+export default HeaderBlog
