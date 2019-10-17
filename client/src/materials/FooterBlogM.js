81ab0a8367262655f312e57588a9eef6bff92713
dancorrea86 <danielmacielcorrea@gmail.com>
2019-10-16 23:22:44 -0300

    commit css grid

--- client/src/materials/FooterBlogM.js
+++ client/src/materials/FooterBlogM.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Footer, CollapsibleItem } from 'react-materialize';
+import { Footer } from 'react-materialize';
 
 const FooterBlobM = props => {
   return (
     <div className="footer-blog-m">
-      <Footer moreLinks={<a />}links={<ul />} className="example">
+      <Footer>
         <h5 className="white-text">Footer Content</h5>
         <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
       </Footer> */}
--- client/src/materials/HeaderBlogM.js
+++ client/src/materials/HeaderBlogM.js
@@ -4,9 +4,10 @@ import { Navbar, NavItem } from 'react-materialize';
 const HeaderBlogM = props => {
   return (
     <div className="header-blog-m">
-      <Navbar >
-        <NavItem href="">Getting started</NavItem>
-        <NavItem href="components.html">Components</NavItem>
+      <Navbar>
+       
+        <NavItem/>Component
+        
       </Navbar>
     </div>
   )
